c27f14848cc370b1329760085d0e7504988e34da
Omar <118364163+19OmarVR@users.noreply.github.com>
2025-10-28 22:56:57 -0600

    Parcial02 - Add app init

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -1,7 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+import { InitService } from '../core/services/init-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -9,6 +11,23 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+
+      return new Promise<void>((resolve) => {
+        setTimeout(async () => {
+          try {
+            return lastValueFrom(initService.init())
+          } finally {
+            const splash = document.getElementById('initial-splash');
+            if (splash) {
+              splash.remove();
+            }
+            resolve();
+          }
+        }, 500);
+      });
+    })
   ]
 };
 
