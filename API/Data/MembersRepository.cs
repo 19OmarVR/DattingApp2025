d97973e271ce411f16938819db3d18d3e7769850
Omar <118364163+19OmarVR@users.noreply.github.com>
2025-10-31 01:15:59 -0600

    Parcial02 - Add JsonIgnore to simulate mapper

--- API/Data/MembersRepository.cs
+++ API/Data/MembersRepository.cs
@@ -13,7 +13,9 @@ namespace API.Data
 
         public async Task<IReadOnlyList<Member>> GetMembersAsync()
         {
-            return await context.Members.ToListAsync();
+            return await context.Members
+            //.Include(m => m.Photos)
+            .ToListAsync();
         }
 
         public async Task<IReadOnlyList<Photo>> GetPhotosAsync(string memberId)
