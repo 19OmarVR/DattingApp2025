184dbba46bd21e8691a7a9c489f116e1acf0d819
Omar <118364163+19OmarVR@users.noreply.github.com>
2026-03-04 13:33:39 -0600

    Parcial04 - Add photo upload

--- src/API/Controllers/MembersController.cs
+++ src/API/Controllers/MembersController.cs
@@ -13,7 +13,7 @@ using System.Security.Claims;
 namespace API.Controllers
 {
     [Authorize]
-    public class MembersController(IMembersRepository membersRepository) : BaseApiController
+    public class MembersController(IMembersRepository membersRepository, IPhotoService photoService) : BaseApiController
     {
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
@@ -60,6 +60,44 @@ namespace API.Controllers
 
             return BadRequest("Failed to update profile");
         }
+        [HttpPost("photo")]
+        public async Task<ActionResult<Photo>> AddPhoto([FromForm] IFormFile file)
+        {
+            var member = await membersRepository.GetMemberForUpdateAsync(User.GetMemberId());
 
+            if (member == null)
+            {
+                return NotFound("Member not found");
+            }
+
+            var result = await photoService.UploadPhotoAsync(file);
+
+            if (result.Error != null)
+            {
+                return BadRequest(result.Error.Message);
+            }
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId,
+                MemberId = User.GetMemberId()
+            };
+
+            if (member.ImageUrl == null)
+            {
+                member.ImageUrl = photo.Url;
+                member.User.ImageUrl = photo.Url;
+            }
+
+            member.Photos.Add(photo);
+
+            if (await membersRepository.SaveAllAsync())
+            {
+                return photo;
+            }
+
+            return BadRequest("Somehting went wrong!");
+        }
     }
 }
