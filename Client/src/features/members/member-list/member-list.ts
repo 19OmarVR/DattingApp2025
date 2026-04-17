6849ffbec53b6b0247d42da3fd3270e10076245e
Omar <118364163+19OmarVR@users.noreply.github.com>
2026-04-16 22:45:22 -0600

    Parcial05 - Add pagination call to API from client

--- Client/src/features/members/member-list/member-list.ts
+++ Client/src/features/members/member-list/member-list.ts
@@ -4,7 +4,8 @@ import { MembersService } from '../../../core/services/members-service';
 import { Member } from '../../../types/member';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
-import { MemberCard } from '../member-card/member-card';
+import { MemberCard } from "../member-card/member-card";
+import { PaginationResult } from '../../../types/paginationMetadata';
 
 @Component({
   selector: 'app-member-list',
@@ -14,9 +15,9 @@ import { MemberCard } from '../member-card/member-card';
 })
 export class MemberList {
   private membersService = inject(MembersService);
-  protected members$: Observable<Member[]>;
+  protected paginatedMembers$: Observable<PaginationResult<Member>>;
 
   constructor() {
-    this.members$ = this.membersService.getMembers();
+    this.paginatedMembers$ = this.membersService.getMembers();
   }
 }
