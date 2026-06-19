271f5115252da2f88caf25a9cefdb1af3ac3259c
Omar <118364163+19OmarVR@users.noreply.github.com>
2026-06-19 17:50:22 -0600

    Parcial06 - Add enhancements to chat

--- Client/src/features/member-messages/member-messages.ts
+++ Client/src/features/member-messages/member-messages.ts
@@ -1,31 +1,68 @@
-import { inject, Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { PaginationResult } from '../../types/paginationMetadata';
+import { Component, effect, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
+import { MessagesService } from '../../core/services/messages-service';
+import { MembersService } from '../../core/services/members-service';
 import { Message } from '../../types/message';
+import { DatePipe } from '@angular/common';
+import { TimeAgoPipe } from '../../core/pipes/time-ago-pipe';
+import { FormsModule } from '@angular/forms';
 
-@Injectable({
-  providedIn: 'root'
+@Component({
+  selector: 'app-member-messages',
+  imports: [DatePipe, TimeAgoPipe, FormsModule],
+  templateUrl: './member-messages.html',
+  styleUrl: './member-messages.css'
 })
-export class MessagesService {
-  private baseUrl = environment.apiUrl;
-  private http = inject(HttpClient);
+export class MemberMessages implements OnInit {
+  @ViewChild('messageEndRef') messageEndRef!: ElementRef;
+  private messagesService = inject(MessagesService);
+  private membersService = inject(MembersService);
+  protected messages = signal<Message[]>([]);
+  protected messageContent = '';
 
-  getMessages(container: string, pageNumber: number, pageSize: number) {
-    let params = new HttpParams();
-
-    params = params.append('pageNumber', pageNumber);
-    params = params.append('pageSize', pageSize);
-    params = params.append('container', container);
-
-    return this.http.get<PaginationResult<Message>>(this.baseUrl + 'messages', { params });
+  constructor() {
+    effect(() => {
+      const currentMessages = this.messages();
+      if (currentMessages.length > 0) {
+        this.scrollToBottom();
+      }
+    });
   }
 
-  getMessageThread(memberId: string) {
-    return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + memberId);
+  ngOnInit(): void {
+    this.loadMessages();
   }
 
-  sendMessage(recipientId: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + 'messages', { recipientId, content });
+  loadMessages() {
+    const memberId = this.membersService.member()?.id;
+    if (memberId) {
+      this.messagesService.getMessageThread(memberId).subscribe({
+        next: messages => this.messages.set(messages.map(message => ({
+          ...message,
+          currentUserSender: message.senderId !== memberId
+        })))
+      })
+    }
+  }
+
+  sendMessage() {
+    const recipientId = this.membersService.member()?.id;
+    if (!recipientId) return;
+    this.messagesService.sendMessage(recipientId, this.messageContent).subscribe({
+      next: message => {
+        this.messages.update(messages => {
+          message.currentUserSender = true;
+          return [...messages, message];
+        });
+        this.messageContent = '';
+      }
+    })
+  }
+
+  scrollToBottom() {
+    setTimeout(() => {
+      if (this.messageEndRef) {
+        this.messageEndRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
   }
 }
