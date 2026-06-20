f282dd036b5cae3b2a651e12496dd78a8dfc4e27
Omar <118364163+19OmarVR@users.noreply.github.com>
2026-06-19 22:20:13 -0600

    Parcial06 - SignalR - Add presence tracker

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -113,7 +113,7 @@ public static class Program
                         {
                             context.Token = accessToken;
                         }
-                        
+
                         return Task.CompletedTask;
                     }
                 };
@@ -144,6 +144,7 @@ public static class Program
         builder.Services.AddScoped<UserActivityLogger>();
         builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
         builder.Services.AddSignalR();
+        builder.Services.AddSingleton<PresenceTracker>();
     }
 
     private static void AddOpenApiDocument(WebApplicationBuilder builder)
