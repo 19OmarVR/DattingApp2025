ed96ef3646fd4026b0f8e3d1ec4aa089aab306ec
Omar <118364163+19OmarVR@users.noreply.github.com>
2026-06-19 22:44:07 -0600

    Parcial06-2 - Add initial migration for Message

--- src/API/Entities/Message.cs
+++ src/API/Entities/Message.cs
@@ -6,7 +6,7 @@ public class Message
     public required string Content { get; set; }
     public DateTime? DateRead { get; set; }
     public DateTime MessageSent { get; set; } = DateTime.UtcNow;
-    public bool SenderDeleted { get; set; }
+    public bool SenderDeleted { get; set; } = true;
     public required string SenderId { get; set; }
     public bool RecipientDeleted { get; set; }
     public required string RecipientId { get; set; }
@@ -14,4 +14,5 @@ public class Message
     // Navigation properties
     public Member Sender { get; set; } = null!;
     public Member Recipient { get; set; } = null!;
+
 }
