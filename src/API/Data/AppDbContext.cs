f5292241aa1fa9888c0ae54a58864eb2a44b9a85
Omar <118364163+19OmarVR@users.noreply.github.com>
2026-06-19 18:21:57 -0600

    Parcial06  - Setup Identity

--- src/API/Data/AppDbContext.cs
+++ src/API/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
@@ -7,7 +8,6 @@ namespace API.Data;
 
 public class AppDbContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
 {
-
     public DbSet<Member> Members { get; set; }
     public DbSet<Photo> Photos { get; set; }
     public DbSet<MemberLike> Likes { get; set; }
@@ -16,6 +16,13 @@ public class AppDbContext(DbContextOptions options) : IdentityDbContext<AppUser>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<IdentityRole>()
+            .HasData(
+                new IdentityRole { Id = "member-id", Name = "Member", NormalizedName = "MEMBER"},
+                new IdentityRole { Id = "moderator-id", Name = "Moderator", NormalizedName = "MODERATOR"},
+                new IdentityRole { Id = "admin-id", Name = "Admin", NormalizedName = "ADMIN"}
+            );
 
         modelBuilder.Entity<MemberLike>()
             .HasKey(m => new { m.SourceMemberId, m.TargetMemberId });
